39aabf7f18d20aca8d82d59d5b8e162bd796928a
Bismark Agyare <bismarkagyare54@gmail.com>
2022-11-07 21:14:34 +0100

    modified book function

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 
 let myLibray = [];
 
-function Book (title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.bookInfo = function() {
-        return `${title} by ${author}, ${pages}, ${read}`
-    }
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.bookInfo = function() {
+    return `${title} by ${author}, ${pages}, ${read}`;
+  }
 }
 
-function addBookToLibrary () {
-    while(true) {
-        let userInput = prompt("add a new book");
-        if (userInput === 'q' || userInput === null) {
-            break;
-        }
 
-        myLibray.push(userInput);
+
+function addBookToLibrary () {
+  while(true) {
+    let userInput = prompt("add a new book");
+    if (userInput === 'q' || userInput === null) {
+      break;
     }
+
+    myLibray.push(userInput);
+  }
 }
